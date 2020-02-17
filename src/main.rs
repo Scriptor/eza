f432ea247e5dc183d30870653dcf7e671e45166e
Scriptor <historium@gmail.com>
2020-02-17 17:59:24 -0500

    Look up keys in db file.
    When a key's value is requested Eza will iterate through
    each line in the DB file until it has the latest entry
    for the given key.
    
    This approach is inefficient and intended to pave the way
    for a system where the file scan only happens when Eza
    starts up and loads the database.
    
    Closes #1

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-use std::collections::HashMap;
 use rocket::State;
-use std::sync::RwLock;
+use std::collections::HashMap;
 use std::fs::{File, OpenOptions};
-use std::io::{self, BufRead, BufWriter, Result, Write};
+use std::io::prelude::*;
+use std::io::{self, BufRead, BufWriter, Result, SeekFrom, Write};
+use std::sync::RwLock;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -14,7 +16,7 @@ fn index() -> &'static str {
 }
 
 struct DBState {
-    map: HashMap<String,String>,
+    map: HashMap<String, String>,
     file: File,
 }
 
@@ -24,22 +26,30 @@ fn persist_entry(db: &DBState, key: &String, value: &String) -> Result<()> {
     w.flush()
 }
 
-fn lookup_entry(db: &DBState, key: &String) {
-    let file = &db.file;
-    let lines = io::BufReader::new(file).lines();
-    for line in lines {
-        if let Ok(entry) = line {
-
+fn lookup_entry<'a>(db: &'a DBState, key: &String) -> Option<String> {
+    let mut file = &db.file;
+    file.seek(SeekFrom::Start(0)).unwrap();
+    let buf = io::BufReader::new(file);
+    let mut result = None;
+    for line in buf.lines() {
+        let entry = line.unwrap();
+        println!("line: {}", &entry);
+        let parts: Vec<&str> = entry.split(":").collect();
+        let line_key = parts[0];
+        if key == line_key {
+            result = Some(String::from(parts[1]));
         }
     }
+    result
 }
 
 #[get("/get/<key>")]
 fn get(state: State<RwLock<DBState>>, key: String) -> String {
     let state = state.read().unwrap();
-    match state.map.get(&key) {
+    let val = lookup_entry(&state, &key);
+    match val {
         Some(s) => format!("Value is: {}", s),
-        _ => String::from("Not found!")
+        None => String::from("Not found!"),
     }
 }
 
@@ -57,11 +67,15 @@ fn main() {
     let file = OpenOptions::new()
         .create(true)
         .read(true)
-        .write(true)
+        .append(true)
         .open("data.db")
         .unwrap();
 
     rocket::ignite()
-        .manage(RwLock::new(DBState{map: memory_db, file: file}))
-        .mount("/", routes![index, set, get]).launch();
+        .manage(RwLock::new(DBState {
+            map: memory_db,
+            file: file,
+        }))
+        .mount("/", routes![index, set, get])
+        .launch();
 }
