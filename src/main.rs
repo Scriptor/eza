ae0d917671b31fcaf3b0e2108e40ec1e9f8cce9d
Scriptor <historium@gmail.com>
2020-02-27 15:46:23 -0500

    Move db-specific code into own module.
    This creates a module that encapsulates the overal "db api".
    The web-specific code has now been separated from the db-specific
    parts.

--- src/main.rs
+++ src/main.rs
@@ -6,55 +6,71 @@ extern crate rocket;
 use rocket::State;
 use std::collections::HashMap;
 use std::fs::{File, OpenOptions};
-use std::io::{self, BufRead, BufWriter, Result, Write};
 use std::sync::RwLock;
+
+mod db {
+    use std::collections::HashMap;
+    use std::fs::File;
+    use std::io::{self, BufRead, BufWriter, Result, Write};
+
+    pub struct DBState {
+        pub map: super::HashMap<String, String>,
+        pub file: super::File,
+    }
+
+    pub fn initialize_db(file: File) -> DBState {
+        let mut map: HashMap<String, String> = HashMap::new();
+        let buf = io::BufReader::new(&file);
+        for line in buf.lines() {
+            let entry = line.unwrap();
+            println!("line: {}", &entry);
+            let parts: Vec<&str> = entry.split(":").collect();
+            if parts.len() == 2 {
+                map.insert(String::from(parts[0]), String::from(parts[1]));
+            }
+        }
+        DBState {
+            map: map,
+            file: file,
+        }
+    }
+
+    fn persist_entry(db: &DBState, key: &String, value: &String) -> Result<()> {
+        let mut w = BufWriter::new(&db.file);
+        writeln!(w, "{}:{}", key, value).unwrap();
+        w.flush()
+    }
+
+    pub fn set(db: &mut DBState, key: String, value: String) -> String {
+        let result_str = format!("Set key: {} to value: {}", key, value);
+        persist_entry(db, &key, &value).unwrap();
+        db.map.insert(key, value);
+        result_str
+    }
+
+    pub fn get(db: &DBState, key: String) -> String {
+        match db.map.get(&key) {
+            Some(s) => format!("Value is: {}", s),
+            _ => String::from("Not found!"),
+        }
+    }
+}
 
 #[get("/")]
 fn index() -> &'static str {
     "Eza DB!"
 }
 
-struct DBState {
-    map: HashMap<String, String>,
-    file: File,
-}
-
-fn persist_entry(db: &DBState, key: &String, value: &String) -> Result<()> {
-    let mut w = BufWriter::new(&db.file);
-    writeln!(w, "{}:{}", key, value).unwrap();
-    w.flush()
-}
-
 #[get("/get/<key>")]
-fn get(state: State<RwLock<DBState>>, key: String) -> String {
-    let state = state.read().unwrap();
-    match state.map.get(&key) {
-        Some(s) => format!("Value is: {}", s),
-        _ => String::from("Not found!"),
-    }
+fn get(state: State<RwLock<db::DBState>>, key: String) -> String {
+    let db = state.read().unwrap();
+    db::get(&db, key)
 }
 
 #[get("/set/<key>/<value>")]
-fn set(state: State<RwLock<DBState>>, key: String, value: String) -> String {
+fn set(state: State<RwLock<db::DBState>>, key: String, value: String) -> String {
     let mut db = state.write().unwrap();
-    let result_str = format!("Set key: {} to value: {}", key, value);
-    persist_entry(&db, &key, &value).unwrap();
-    db.map.insert(key, value);
-    result_str
-}
-
-fn load_db(file: &File) -> HashMap<String, String> {
-    let mut map: HashMap<String, String> = HashMap::new();
-    let buf = io::BufReader::new(file);
-    for line in buf.lines() {
-        let entry = line.unwrap();
-        println!("line: {}", &entry);
-        let parts: Vec<&str> = entry.split(":").collect();
-        if parts.len() == 2 {
-            map.insert(String::from(parts[0]), String::from(parts[1]));
-        }
-    }
-    map
+    db::set(&mut db, key, value)
 }
 
 fn main() {
@@ -64,13 +80,10 @@ fn main() {
         .append(true)
         .open("data.db")
         .unwrap();
-    let memory_db = load_db(&file);
+    let db = db::initialize_db(file);
 
     rocket::ignite()
-        .manage(RwLock::new(DBState {
-            map: memory_db,
-            file: file,
-        }))
+        .manage(RwLock::new(db))
         .mount("/", routes![index, set, get])
         .launch();
 }
